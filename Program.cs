5f2fed74ec081950a328da00592ebb07a31f99bd
Savenok <Romasav2017@gmail.com>
2022-08-31 09:52:18 +0300

    // . Напишите программу, которая будет выдавать название дня недели по заданному номеру 2.0

--- Program.cs
+++ Program.cs
@@ -1,39 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// 
+// . Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+// Если честно то данную идея увидел у ребят и воплотил в более крутое  и красивое :)
 Console.WriteLine("Введите день: ");
 int day = int.Parse(Console.ReadLine());
-List<string> title_day_in_week = new List<string>() { "Понедельник", "Вторник", "Cреда" , "Четверг"  , "Пятница"  ,"Суббота"  ," Воскресенье"  };
-if(day == 1)
+string[] array = new string[] { "Понедельник" , "Вторник" , "Среда" , "Четверг" , "Пятница" , "Суббота" , "Воскресенье" };
+if(day > 0 && day < 8)
 {
-     Console.WriteLine(title_day_in_week[0]);
-}
-else if(day == 2)
-{
-    Console.WriteLine(title_day_in_week[1]);
-}
-else if(day == 3)
-{
-    Console.WriteLine(title_day_in_week[2]);
-}
-else if(day == 4)
-{
-    Console.WriteLine(title_day_in_week[3]);
-}
-else if(day == 5)
-{
-    Console.WriteLine(title_day_in_week[4]);
-}
-else if(day == 6)
-{
-    Console.WriteLine(title_day_in_week[5]);
-}
-else if(day == 7)
-{
-    Console.WriteLine(title_day_in_week[6]);
+    Console.WriteLine($"{day} день - {array[day-1]}");
 }
 else
 {
-    Console.WriteLine("Такого дня не существует");
+    Console.WriteLine("Такого не существует");
 }
-
-

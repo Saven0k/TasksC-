b06292ce66f22b5d3872df13985810207f518840
Savenok <Romasav2017@gmail.com>
2022-08-31 09:45:26 +0300

    2.0

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Напишите программу , которая будет выдавать название дня недели по заданному номеру.
-// Сделал по красоте,знаю что можно было лучше и быстрее но сделал красиво :)
+// 
 Console.WriteLine("Введите день: ");
 int day = int.Parse(Console.ReadLine());
 List<string> title_day_in_week = new List<string>() { "Понедельник", "Вторник", "Cреда" , "Четверг"  , "Пятница"  ,"Суббота"  ," Воскресенье"  };
@@ -36,3 +35,5 @@ else
 {
     Console.WriteLine("Такого дня не существует");
 }
+
+

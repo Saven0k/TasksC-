a2dbf8e742f93618fca7650f9e6af929d81399cb
Savenok <Romasav2017@gmail.com>
2022-08-31 09:35:36 +0300

    Напишите программу , которая будет выдавать название дня недели по заданному номеру.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Напишите программу , которая будет принимать на вход 2 числа, и проверять не являеться ли одно квадратом другого
-Console.WriteLine("Введите  первое число: ");
-int sqr = int.Parse( s :Console.ReadLine());
-Console.WriteLine("Введите  второе число: ");
-int number = int.Parse( s :Console.ReadLine());
-if(sqr == number*number)
+// Напишите программу , которая будет выдавать название дня недели по заданному номеру.
+// Сделал по красоте,знаю что можно было лучше и быстрее но сделал красиво :)
+Console.WriteLine("Введите день: ");
+int day = int.Parse(Console.ReadLine());
+List<string> title_day_in_week = new List<string>() { "Понедельник", "Вторник", "Cреда" , "Четверг"  , "Пятница"  ,"Суббота"  ," Воскресенье"  };
+if(day == 1)
 {
-    Console.WriteLine($"Число  {sqr} являеться квадратом числа {number}");
+     Console.WriteLine(title_day_in_week[0]);
+}
+else if(day == 2)
+{
+    Console.WriteLine(title_day_in_week[1]);
+}
+else if(day == 3)
+{
+    Console.WriteLine(title_day_in_week[2]);
+}
+else if(day == 4)
+{
+    Console.WriteLine(title_day_in_week[3]);
+}
+else if(day == 5)
+{
+    Console.WriteLine(title_day_in_week[4]);
+}
+else if(day == 6)
+{
+    Console.WriteLine(title_day_in_week[5]);
+}
+else if(day == 7)
+{
+    Console.WriteLine(title_day_in_week[6]);
 }
 else
 {
-    Console.WriteLine($"Число {sqr} не являеться квадратом числа {number}");
+    Console.WriteLine("Такого дня не существует");
 }
-
-
-
-
-
-
-

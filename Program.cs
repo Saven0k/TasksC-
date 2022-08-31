1e8900f95bed8d753f09e1edfe14a2e997261c43
Savenok <Romasav2017@gmail.com>
2022-08-31 10:16:10 +0300

    -N до N

--- Program.cs
+++ Program.cs
@@ -1,38 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// Напишите программу , которая будет выдавать название дня недели по заданному номеру.
-// Сделал по красоте,знаю что можно было лучше и быстрее но сделал красиво :)
-Console.WriteLine("Введите день: ");
-int day = int.Parse(Console.ReadLine());
-List<string> title_day_in_week = new List<string>() { "Понедельник", "Вторник", "Cреда" , "Четверг"  , "Пятница"  ,"Суббота"  ," Воскресенье"  };
-if(day == 1)
+//5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+//4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+//2 -> " -2, -1, 0, 1,2"
+Console.WriteLine("Введите число");
+int a = int.Parse( s :Console.ReadLine());
+int b = a * (-1);
+while (b<=a)
 {
-     Console.WriteLine(title_day_in_week[0]);
+    Console.Write(b + " , ");
+    b++;
 }
-else if(day == 2)
-{
-    Console.WriteLine(title_day_in_week[1]);
-}
-else if(day == 3)
-{
-    Console.WriteLine(title_day_in_week[2]);
-}
-else if(day == 4)
-{
-    Console.WriteLine(title_day_in_week[3]);
-}
-else if(day == 5)
-{
-    Console.WriteLine(title_day_in_week[4]);
-}
-else if(day == 6)
-{
-    Console.WriteLine(title_day_in_week[5]);
-}
-else if(day == 7)
-{
-    Console.WriteLine(title_day_in_week[6]);
-}
-else
-{
-    Console.WriteLine("Такого дня не существует");
-}
+
+// Данное решение ъоть и глупое но очень быстрое и удобное

b5a98b5297de016a2c2c850e6c51cb37e911e75d
Savenok <Romasav2017@gmail.com>
2022-08-31 09:09:56 +0300

    Напишите программу , которая будет принимать на вход 2 числа, и проверять не являеться ли одно квадратом другого

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите  число: ");
-float number = float.Parse( s :Console.ReadLine());
+// Напишите программу , которая будет принимать на вход 2 числа, и проверять не являеться ли одно квадратом другого
+Console.WriteLine("Введите  первое число: ");
+int sqr = int.Parse( s :Console.ReadLine());
+Console.WriteLine("Введите  второе число: ");
+int number = int.Parse( s :Console.ReadLine());
 
-float srt = number % 2;
+int result = number * number;
 
-
-if(srt == 0)
+if(result == sqr)
 {
-    Console.WriteLine("Да, оно четное");
-
-} 
+    Console.WriteLine($"Число  {sqr} являеться квадратом числа {number}");
+}
 else
 {
-    Console.WriteLine("Нет, оно не четное");
+    Console.WriteLine($"Число {sqr} не являеться квадратом числа {number}");
 }
+
 
 
 
